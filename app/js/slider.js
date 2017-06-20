02a99016e5766f4da64215acdc685b9cba5b90bc
Leonardo Cardoso <leocardosoti@gmail.com>
2017-06-20 12:42:36 -0300

    Alterações finais

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,18 +1,37 @@
- var myIndex = 0;
- carousel();
+var myIndex = 0;
+var slideIndex = 1;
+var bullets = document.getElementsByClassName("bullet");
+var sliders = document.getElementsByClassName("slider");
 
+for (var i = 0; i < bullets.length; i++){
+	bullets[i].onclick = function() {
+		slideManual(this.getAttribute("value"));
+	};
+}
 
- function carousel() {
-  var i;
-  var sliders = document.getElementsByClassName("slider");
-  var bullets = document.getElementsByClassName("bullet");
-  for (i = 0; i < sliders.length; i++) {
-    sliders[i].style.display = "none";  
-   bullets[i].style.opacity = ".5";
- }
- myIndex++;
- if (myIndex > sliders.length) {myIndex = 1}    
-  sliders[myIndex-1].style.display = "block"; 
-bullets[myIndex-1].style.opacity = "1"; 
-      setTimeout(carousel, 5000); // Change image every 2 seconds
-    }
+slide();
+
+function slide() {	
+	for (var i = 0; i < sliders.length; i++) {
+		sliders[i].style.display = "none";  
+		bullets[i].style.opacity = ".5";
+	}
+	myIndex++;
+	if (myIndex > sliders.length) {
+		myIndex = 1
+	}    
+	sliders[myIndex-1].style.display = "block"; 
+	bullets[myIndex-1].style.opacity = "1"; 
+	setTimeout(slide, 10000); 
+}
+
+function slideManual(n) {
+	
+	for (var i = 0; i < sliders.length; i++) {
+		sliders[i].style.display = "none";  
+		bullets[i].style.opacity = ".5"; 
+	}
+
+	sliders[n].style.display = "block"; 
+	bullets[n].style.opacity = "1"; 
+}
